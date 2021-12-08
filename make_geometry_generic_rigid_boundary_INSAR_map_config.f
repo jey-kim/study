08e8bf7cdcb932ad83c687be7f80e4e7df566e0d
Jay <jeonghyeop.kim@gmail.com>
2021-12-08 18:55:30 -0500

    added make_geometry_kin_map_config.f & make_geometry_generic_rigid_boundary_INSAR_map_config.f

---
+++ make_geometry_generic_rigid_boundary_INSAR_map_config.f
@@ -0,0 +1,78 @@
+	program make_grid
+
+        implicit real*8 (a-h,o-z)
+
+        open(11, file = 'map_config.txt')
+
+        read(11,*)rlon_min
+        read(11,*)rlon_max
+        read(11,*)rlat_min
+        read(11,*)rlat_max
+        read(11,*)step
+
+        float_nx=(rlon_max-rlon_min)/step
+        nx = nint(float_nx)
+
+        float_ny=(rlat_max-rlat_min)/step
+        ny = nint(float_ny)
+
+	open(22, file = 'geometry_rigid_boundary_test.dat')
+
+	write(22,*)nx,ny,2
+
+        rlong = rlon_min
+	rlat = rlat_min
+        ix = 0
+	iy = 0
+
+                do j = 1, nx+1 
+
+                     write(22,*)ix,iy,2,0,3
+                     write(22,25)rlat,rlong
+25                   format(2f12.5)
+                        rlong = rlong + step
+		     ix = ix + 1
+
+                end do 
+        iy = 1
+        rlat = rlat_min + step
+	do i = 1,ny-1 
+
+	ix = 0
+	rlong = rlon_min
+           write(22,*)ix,iy,2,0,3
+           write(22,25)rlat,rlong
+           ix = ix + 1
+           rlong = rlong + step
+		do j = 2, nx 
+
+		     write(22,*)ix,iy,1,3,3
+		     write(22,25)rlat,rlong
+		     ix = ix + 1
+                        rlong = rlong + step
+		
+		end do
+           write(22,*)ix,iy,2,0,3
+           write(22,25)rlat,rlong
+           ix = ix + 1
+
+                
+	    rlat = rlat + step
+	    iy = iy + 1
+
+	end do
+        ix = 0
+        rlong = rlon_min
+                do j = 1, nx + 1
+
+                     write(22,*)ix,iy,2,0,3
+                     write(22,25)rlat,rlong
+                        rlong = rlong + step
+                     ix = ix + 1
+                end do
+
+	stop
+
+	end
+
+
